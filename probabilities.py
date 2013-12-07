23220757811145a78bd1a64c536128c219b3f5dd
Louis Impagliazzo <imp@gliazzo.com>
2013-12-07 03:44:21 -0600

    added function to retrieve probability of words given tag

--- probabilities.py
+++ probabilities.py
@@ -1,5 +1,4 @@
 import ujson
-import ngramanalysis
 
 
 def writeptag():
@@ -9,10 +8,9 @@ def writeptag():
                      .replace(';', '').replace(':', '').replace('"', ''): j
                      for i, j in tagdata.items() if j >= 1000}
         total_count = sum(freq_tags.values())
-        probs = {word: value/total_count for word, value in freq_tags}
+        probs = {word: value/total_count for word, value in freq_tags.items()}
         with open('ptags1000.json', 'w') as write:
             ujson.dump(probs, write)
-
 
 
 def writepwords():
@@ -25,3 +23,25 @@ def writepwords():
             ujson.dump(probs, write)
 
 
+def writepwordgiventag():
+    with open('tagwordcounts.json', 'r') as file:
+        stuff = ujson.load(file)
+        pwordtagdict = {}
+        with open('tags.json') as tags:
+            tagdict = ujson.load(tags)
+            for tag in stuff.keys():
+                #dictionary of words and counts for particular tag
+                worddict = stuff[tag]
+                total_count = tagdict[tag]
+                probdict = {word: value/total_count for word, value in worddict.items()}
+                pwordtagdict[tag] = probdict
+        with open('wordgiventag1000.json', 'w') as write:
+            ujson.dump(pwordtagdict, write)
+
+
+def pwordgiventag(tag):
+    with open('wordgiventag1000.json', 'r') as file:
+        stuff = ujson.load(file)
+    return stuff[tag]
+
+print(sorted(pwordgiventag('python').items(), key=lambda x: x[1], reverse=True)[:100])
