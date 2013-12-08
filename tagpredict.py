9bd115de87279182df0ae4ba019553067ffedee3
Louis Impagliazzo <imp@gliazzo.com>
2013-12-08 00:41:32 -0600

    writes probabilities to file, need to tweak prob function a bit

--- tagpredict.py
+++ tagpredict.py
@@ -12,11 +12,26 @@ def tagsover(n):
                 for i, j in tagdata.items() if j >= n]
     return tags
 
+'''if __name__ == "__main__":
+    with open('probabilitiestrain.json') as readfile:
+        stuff = ujson.load(readfile)
+        print(sorted(stuff['6860135'].items(), key=lambda x: x[1], reverse=True)[:10])'''
+
+
 if __name__ == "__main__":
-    with open('Test.csv', newline='') as csvfile:
-        csvreader = csv.reader(csvfile, newline='')
+    tag_list = tagsover(400)
+    with open('wordgroup400plus.json', 'r') as readfile:
+        idworddict = ujson.load(readfile)
+        wordgroups = list(idworddict.values())
+        probabilities = bayes(tag_list, wordgroups)
+        with open('probabilities.json', 'w') as write:
+            ujson.dump(probabilities, write)
+
+'''if __name__ == "__main__":
+    with open('Test.csv') as csvfile:
+        csvreader = csv.reader(csvfile)
         data_dict = {}
-        tags = tagsover(1000)
+        tags = tagsover(400)
         for row in csvreader:
             id = row[0]
             print(id)
@@ -24,3 +39,5 @@ if __name__ == "__main__":
                         .replace('.', '').replace(';', '').replace(',', '')
                         .replace('?', '').replace(';', '').replace(':', '').split())
             data_dict[id] = words
+        with open('wordgroup400plus.json', 'w') as write:
+            ujson.dump(data_dict, write)'''
