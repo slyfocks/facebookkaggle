9e74ab32f5891d5c02758d00d16917f3fcdc0eab
Louis Impagliazzo <imp@gliazzo.com>
2013-12-08 02:26:22 -0600

    changed bayes function to output tags based on bayes ratio

--- probabilities.py
+++ probabilities.py
@@ -1,80 +1,88 @@
 import ujson
 import math
-from functools import reduce
+
+
+def tagsover(n):
+    with open('tags.json') as file:
+        tagdata = ujson.load(file)
+        tags = {i.replace('.', '').replace(',', '').replace('?', '')
+                .replace(';', '').replace(':', '').replace('"', ''): j
+                for i, j in tagdata.items() if j >= n}
+    return tags
+
 
 def writeptag():
     with open('tags.json') as file:
         tagdata = ujson.load(file)
         freq_tags = {i.replace('.', '').replace(',', '').replace('?', '')
                      .replace(';', '').replace(':', '').replace('"', ''): j
-                     for i, j in tagdata.items() if j >= 1000}
+                     for i, j in tagdata.items() if j >= 400}
         total_count = sum(freq_tags.values())
         probs = {word: value/total_count for word, value in freq_tags.items()}
-        with open('ptags1000.json', 'w') as write:
+        with open('ptags400plus.json', 'w') as write:
             ujson.dump(probs, write)
 
 
 def writepwords():
-    with open('wordcount1000.json', 'r') as file:
+    with open('wordcount400plus.json', 'r') as file:
         stuff = ujson.load(file)
         total_count = sum(stuff.values())
         #takes individual word totals and divides them by global total to compute probability for each word
         probs = {word: value/total_count for word, value in stuff.items()}
-        with open('pwords1000.json', 'w') as write:
+        with open('pwords400plus.json', 'w') as write:
             ujson.dump(probs, write)
 
 
 def writepwordgiventag():
-    with open('tagwordcounts.json', 'r') as file:
+    with open('tagwordcount400plus.json', 'r') as file:
         stuff = ujson.load(file)
         pwordtagdict = {}
-        with open('tags.json') as tags:
-            tagdict = ujson.load(tags)
-            for tag in stuff.keys():
-                #dictionary of words and counts for particular tag
-                worddict = stuff[tag]
-                total_count = tagdict[tag]
-                probdict = {word: value/total_count for word, value in worddict.items()}
-                pwordtagdict[tag] = probdict
-        with open('wordgiventag1000.json', 'w') as write:
-            ujson.dump(pwordtagdict, write)
+        tagdict = tagsover(400)
+        for tag in stuff.keys():
+            #dictionary of words and counts for particular tag
+            worddict = stuff[tag]
+            total_count = tagdict[tag]
+            probdict = {word: value/total_count for word, value in worddict.items()}
+            pwordtagdict[tag] = probdict
+    with open('wordgiventag400plus.json', 'w') as write:
+        ujson.dump(pwordtagdict, write)
 
 
 #return prior probability of tags
 def ptag(tags):
-    with open('ptags1000.json', 'r') as file:
+    with open('ptags400plus.json', 'r') as file:
         ptagdict = ujson.load(file)
     return {tag: ptagdict[tag] for tag in tags}
 
 
 def pword():
-    with open('pwords1000.json', 'r') as file:
+    with open('pwords400plus.json', 'r') as file:
         pworddict = ujson.load(file)
     return pworddict
 
 
 def pwordgiventag(tags):
-    with open('wordgiventag1000.json', 'r') as file:
+    with open('wordgiventag400plus.json', 'r') as file:
         postprobs = ujson.load(file)
     return {tag: postprobs[tag] for tag in tags}
 
 
 #in the form of P(tags|words) where tags and wordgroups are lists of lists of words
+#doesn't give reasonable probabilities in absolute terms, but only relative probability matters here
 def bayes(tags, wordgroups):
     postdict = pwordgiventag(tags)
     ptagdict = ptag(tags)
     pworddict = pword()
     probability = {}
     #index words in wordgroups
-    id = 0
+    id = 6034196
     for words in wordgroups:
-        probability[id] = {}
+        probability[id] = []
         for tag in tags:
-            probability[id][tag] = 0
             #count number of words that have been associated with given tag in training set
-            i = 0
-            posterior = 1
-            bayesratio = 1
+            #i = 0
+            #bayesratio = 1
+            priortag = ptagdict[tag]
             for word in words:
                 try:
                     priorword = pworddict[word]
@@ -84,18 +92,20 @@ def bayes(tags, wordgroups):
                 try:
                     #updates posterior probability of words|tag naively
                     posterior = postdict[tag][word]
-                    bayesratio *= posterior/priorword
-                    i += 1
+                    bayesratio = posterior/priorword
+                    #i += 1
+                    #for a safer predictor that weights tag commonality more heavily, increase exponent
+                    if bayesratio*priortag**1.2 >= 0.5:
+                        probability[id].append(tag)
                 except KeyError:
                     continue
-            if i != 0:
-                bayesratio = math.pow(posterior, 1/i)
+            '''if i != 0:
+                bayesratio = math.pow(bayesratio, 1/i)
             else:
                 bayesratio = 0
-            priortag = ptagdict[tag]
-            print(posterior, priortag, priorword, i)
-            probability[id][tag] = bayesratio*priortag
+            probability[id][tag] = bayesratio*priortag'''
+        probability[id] = set(probability[id])
         id += 1
+        print(id)
     return probability
-
-print(bayes(['python', 'c#', 'android'], [['code', 'python'], ['monkey']]))
+print(bayes(list(tagsover(400).keys()), [['getting', 'rid', 'site-specific', 'hotkeys'], ['html']]))
