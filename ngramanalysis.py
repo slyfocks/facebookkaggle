cbb0fa6415747d6ce1edc37b22c53e6f6e8086e8
Louis Impagliazzo <imp@gliazzo.com>
2013-12-08 19:15:51 -0600

    add body word count dicts together

--- ngramanalysis.py
+++ ngramanalysis.py
@@ -3,7 +3,7 @@ from collections import Counter
 
 
 def ngramcount():
-    with open('titlewordcount400to500.json') as file:
+    with open('tagwordcount200plus.json') as file:
         stuff = ujson.load(file)
         keys = stuff.keys()
         counts = {}
@@ -13,7 +13,7 @@ def ngramcount():
             for word in words:
                 cnt[word] += 1
             counts[key] = cnt
-        with open('tagwordcount400to500.json', 'w') as write:
+        with open('tagwordcount200.json', 'w') as write:
             ujson.dump(counts, write)
 
 
@@ -27,35 +27,27 @@ def pythoncounttest():
 
 #count the instances of each word among all tags
 def globalwordcount():
-    with open('tagwordcount400plus.json', 'r') as file:
+    with open('tagwordcount200.json', 'r') as file:
         stuff = ujson.load(file)
         wordcounts = stuff.values()
         cnt = Counter()
         for dicts in wordcounts:
             for word, count in dicts.items():
                 cnt[word] += count
-        with open('wordcount400plus.json', 'w') as write:
+        with open('wordcount200plus.json', 'w') as write:
             ujson.dump(cnt, write)
 
 
 def combine_dicts():
-    with open('tagwordcounts.json') as tag1000:
-        tag1000dict = ujson.load(tag1000)
-        with open('tagwordcount900to1000.json') as tag900:
-            tag900dict = ujson.load(tag900)
-            with open('tagwordcount800to900.json') as tag800:
-                tag800dict = ujson.load(tag800)
-                with open('tagwordcount700to800.json') as tag700:
-                    tag700dict = ujson.load(tag700)
-                    with open('tagwordcount600to700.json') as tag600:
-                        tag600dict = ujson.load(tag600)
-                        with open('tagwordcount500to600.json') as tag500:
-                            tag500dict = ujson.load(tag500)
-                            with open('tagwordcount400to500.json') as tag400:
-                                tag400dict = ujson.load(tag400)
-                                newdict = dict(list(tag1000dict.items()) + list(tag900dict.items())
-                                               + list(tag800dict.items()) + list(tag700dict.items())
-                                               + list(tag600dict.items()) + list(tag500dict.items())
-                                               + list(tag400dict.items()))
-                                with open('tagwordcount400plus.json', 'w') as write:
-                                    ujson.dump(newdict, write)
+    with open('bodytagcount10000to20000.json.json') as tag10000:
+        tag10000dict = ujson.load(tag10000)
+        with open('bodytagcount5000to10000.json.json') as tag5000:
+            tag5000dict = ujson.load(tag5000)
+            with open('bodytagcount500to5000.json.json') as tag500:
+                tag500dict = ujson.load(tag500)
+                with open('bodytagcount200to500.json') as tag200:
+                    tag200dict = ujson.load(tag200)
+                    newdict = dict(list(tag10000dict.items()) + list(tag5000dict.items())
+                                   + list(tag500dict.items()) + list(tag200dict.items()))
+                    with open('tagwordcount400plus.json', 'w') as write:
+                        ujson.dump(newdict, write)
