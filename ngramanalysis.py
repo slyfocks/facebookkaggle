ab1698e7a26fdadf3c3d4b948ea90ab883e96bb2
Louis Impagliazzo <imp@gliazzo.com>
2013-12-10 05:46:56 -0600

    finished creating file with tags and words with over 10 instances

--- ngramanalysis.py
+++ ngramanalysis.py
@@ -4,7 +4,7 @@ from itertools import chain
 
 
 def ngramcount():
-    with open('bodytagwordcount325000to450000.json') as file:
+    with open('bodytagwordcount400000to450000.json') as file:
         stuff = ujson.load(file)
         keys = stuff.keys()
         counts = {}
@@ -14,7 +14,7 @@ def ngramcount():
             for word in words:
                 cnt[word] += 1
             counts[key] = cnt
-        with open('bodytagwordcounts325000.json', 'w') as write:
+        with open('bodytag400000to450000.json', 'w') as write:
             ujson.dump(counts, write)
 
 
@@ -28,32 +28,35 @@ def pythoncounttest():
 
 #count the instances of each word among all tags
 def globalwordcount():
-    with open('bodytag8000toinfinityover2.json', 'r') as file:
+    with open('bodytagsover100.json', 'r') as file:
         stuff = ujson.load(file)
         wordcounts = stuff.values()
         cnt = Counter()
         for dicts in wordcounts:
             for word, count in dicts.items():
                 cnt[word] += count
-        with open('globalwordcount8000toinfinityover2.json', 'w') as write:
+        with open('globalwordcountover100.json', 'w') as write:
             ujson.dump(cnt, write)
 
 
 def combine_dicts():
-    with open('bodytag8000to30000over2.json') as tag200, open('bodytag30000to450000plusover2.json') as tag1500:
+    with open('bodytag400000to450000over10.json') as tag200, open('bodytagsover10.json') as tag1500:
         tag200dict = ujson.load(tag200)
         tag500dict = ujson.load(tag1500)
         newdict = dict(chain(tag200dict.items(), tag500dict.items()))
-        with open('bodytag8000toinfinityover2.json', 'w') as write:
+        with open('bodytagsover10.json', 'w') as write:
             ujson.dump(newdict, write)
 
 
 #deletes words that appear less than n times to save memory
 def delete_below(n):
-    with open('pbodywords200plus.json') as file:
+    with open('bodytagsover10.json') as file:
         boop = ujson.load(file)
-        newdict = {word: boop[word] for word in boop.keys() if int(boop[word]) > n}
-        with open('bodytag30000to450000plusover2.json', 'w') as write:
+        newdict = {}
+        for tag in boop.keys():
+            newdict[tag] = {}
+            for word in boop[tag].keys():
+                if int(boop[tag][word]) > n:
+                    newdict[tag][word] = boop[tag][word]
+        with open('bodytagsover100.json', 'w') as write:
             ujson.dump(newdict, write)
-
-delete_below(1)
