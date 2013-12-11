61027994c83a5700663c955d2b3f563f00edaf8d
Louis Impagliazzo <imp@gliazzo.com>
2013-12-11 02:31:21 -0600

    now predicting based on a comparison of train titles to test body

--- tagpredict.py
+++ tagpredict.py
@@ -1,7 +1,7 @@
 import ujson
 from probabilities import bayes
 import csv
-
+import ast
 
 #takes tags with n or more instances. have to limit this to maintain computational tractability
 def tagsover(n):
@@ -21,8 +21,9 @@ def tagsbetween(a, b):
                 for i, j in tagdata.items() if b > j >= a]
     return tags
 
+
 def chosentags():
-    with open('probabilities200plus.json') as readfile:
+    with open('probabilities.json') as readfile:
         stuff = ujson.load(readfile)
         return stuff
 
@@ -32,37 +33,36 @@ def chosentags2():
         stuff = ujson.load(readfile)
         return stuff
 
+
 def writetags():
-    tag_list = tagsover(200)
-    with open('wordgroup200plus.json', 'r') as readfile:
-        idworddict = ujson.load(readfile)
+    tag_list = tagsover(10)
+    with open('titlewordgroup.json') as file:
+        filedict = ujson.load(file)
         #makes sure wordgroups are in correct order because dictionaries are unordered
-        wordgroups = [v for k, v in sorted(idworddict.items())]
-        probabilities = bayes(tag_list, wordgroups)
-        with open('probabilitiestest.json', 'w') as write:
-            ujson.dump(probabilities, write)
-#writetags()
+        wordgroups = [v for k, v in sorted(filedict.items())][100:1000]
+        bayes(tag_list, wordgroups)
+writetags()
 #print(chosentags()['6034218'])
-if __name__ == "__main__":
+'''if __name__ == "__main__":
     with open('Test.csv') as csvfile:
         csvreader = csv.reader(csvfile)
-        with open('globalwordcountover100.json') as wordcount:
-            bodywords = set(list(ujson.load(wordcount).keys()))
-            data_dict = {}
-            tags = tagsover(200)
+        with open('titleglobalcount.json') as wordcount:
+            bodywords = set(ujson.load(wordcount).keys())
             stopwords = ['at', 'an' 'which', 'on', 'in', 'the', 'is', 'are',
                          'of', 'my', 'from', 'a', 'or', 'and', 'for', 'do', 'did',
                          'I', 'to', 'too', 'out', 'why', 'what', 'by', 'if', 'this',
-                         'that', 'all', 'any', 'but', 'been', 'so', 'then', 'than']
-            for row in csvreader:
-                id = row[0]
-                print(id)
-                words = set(str(row[1]).lower().replace('"', '')
+                         'that', 'all', 'any', 'but', 'been', 'so', 'then', 'than',
+                         'content', 'id', 'think', 'want', 'know', 'also', 'it', 'am',
+                         'code', 'over', 'under', 'move', 'remove']
+            worddict = {}
+            for i, row in enumerate(csvreader):
+                words = set(str(row[2]).lower().replace('"', '')
                             .replace('.', '').replace(';', '').replace(',', '')
                             .replace('?', '').replace(';', '').replace(':', '').split())
-                data_dict[id] = [word for word in words if word not in stopwords and word in bodywords]
-            with open('bodywordgroup200plusover100.json', 'w') as write:
-                ujson.dump(data_dict, write)
+                worddict[row[0]] = [word for word in words if word not in stopwords and word in bodywords]
+                print(i)
+            with open('titlewordgroup.json', 'w') as write:
+                ujson.dump(worddict, write)'''
 
 
 def create_submission():
