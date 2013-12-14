63436264898d137ea825a60e1a4b0b2a59118ad8
Louis Impagliazzo <imp@gliazzo.com>
2013-12-14 01:26:43 -0600

    final searchcsv

--- searchcsv.py
+++ searchcsv.py
@@ -2,8 +2,8 @@ import csv
 import ujson
 with open('tags.json') as file:
     data = ujson.load(file)
-freq_tags = [i.replace('.', '').replace(',', '').replace('?', '').replace(';', '')
-              .replace(':', '').replace('"', '') for i, j in data.items() if 10000 > j >= 5000]
+freq_tags = [i.replace(',', '').replace('?', '').replace(';', '')
+              .replace(':', '').replace('"', '') for i, j in data.items() if 10000 > j >= 3000]
 data_dict = {}
 print(len(freq_tags))
 with open('/home/buzzybee/facebookrecruit/Train.csv', newline='') as csvfile:
@@ -12,20 +12,22 @@ with open('/home/buzzybee/facebookrecruit/Train.csv', newline='') as csvfile:
     stopwords = ['at', 'an' 'which', 'on', 'in', 'the', 'is', 'are',
                  'of', 'my', 'from', 'a', 'or', 'and', 'for', 'do', 'did',
                  'I', 'to', 'too', 'out', 'why', 'what', 'by', 'if', 'this',
-                 'that', 'all', 'any', 'but', 'been', 'so', 'then', 'than']
+                 'that', 'all', 'any', 'but', 'been', 'so', 'then', 'than',
+                 'content', 'id', 'think', 'want', 'know', 'also', 'it', 'am',
+                 'code', 'over', 'under', 'move', 'remove']
     for ngram in freq_tags:
         data_dict[ngram] = []
     for row in filereader:
         print(i)
         i += 1
-        for tag in set(str(row[3]).lower().replace('"', '').replace('.', '')
+        for tag in set(str(row[3]).lower().replace('"', '')
                                   .replace(',', '').replace('?', '').replace(';', '')
                                   .replace(':', '').split()):
             if tag in freq_tags:
-                for word in set(str(row[2]).lower().replace('"', '').replace('.', '')
+                for word in set(str(row[1]).lower().replace('"', '').replace('.', '')
                                            .replace(';', '').replace(',', '').replace('?', '')
                                            .replace(';', '').replace(':', '').split()):
                     if word not in stopwords:
                         data_dict[tag].append(word)
-with open('bodytagcount5000to10000.json', 'w') as file:
+with open('titlewordcount3000to10000.json', 'w') as file:
     ujson.dump(data_dict, file)
