1bb8e60a2014a41f774c0aac2a7c2cc6a70faa73
Louis Impagliazzo <imp@gliazzo.com>
2013-12-14 01:26:55 -0600

    final ngram

--- ngramanalysis.py
+++ ngramanalysis.py
@@ -4,7 +4,7 @@ from itertools import chain
 
 
 def ngramcount():
-    with open('bodytagwordcount400000to450000.json') as file:
+    with open('titlewordcount3000to10000.json') as file:
         stuff = ujson.load(file)
         keys = stuff.keys()
         counts = {}
@@ -14,7 +14,7 @@ def ngramcount():
             for word in words:
                 cnt[word] += 1
             counts[key] = cnt
-        with open('bodytag400000to450000.json', 'w') as write:
+        with open('title3000to10000.json', 'w') as write:
             ujson.dump(counts, write)
 
 
@@ -28,23 +28,23 @@ def pythoncounttest():
 
 #count the instances of each word among all tags
 def globalwordcount():
-    with open('bodytagsover100.json', 'r') as file:
+    with open('titletagwords.json', 'r') as file:
         stuff = ujson.load(file)
         wordcounts = stuff.values()
         cnt = Counter()
         for dicts in wordcounts:
             for word, count in dicts.items():
                 cnt[word] += count
-        with open('globalwordcountover100.json', 'w') as write:
+        with open('titleglobalcount.json', 'w') as write:
             ujson.dump(cnt, write)
 
 
 def combine_dicts():
-    with open('bodytag400000to450000over10.json') as tag200, open('bodytagsover10.json') as tag1500:
+    with open('title10to100000.json') as tag200, open('title100000plus.json') as tag1500:
         tag200dict = ujson.load(tag200)
         tag500dict = ujson.load(tag1500)
         newdict = dict(chain(tag200dict.items(), tag500dict.items()))
-        with open('bodytagsover10.json', 'w') as write:
+        with open('titletagwords.json', 'w') as write:
             ujson.dump(newdict, write)
 
 
@@ -60,3 +60,5 @@ def delete_below(n):
                     newdict[tag][word] = boop[tag][word]
         with open('bodytagsover100.json', 'w') as write:
             ujson.dump(newdict, write)
+
+globalwordcount()
