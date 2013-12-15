63c14697856e62d26699d9d58b110b1bf71bccfe
Louis Impagliazzo <imp@gliazzo.com>
2013-12-15 04:35:08 -0600

    deleted functions no longer needed

--- tagpredict.py
+++ tagpredict.py
@@ -28,26 +28,14 @@ def chosentags():
         return stuff
 
 
-def chosentags2():
-    with open('probabilities200plus2.json') as readfile:
-        stuff = ujson.load(readfile)
-        return stuff
-
-
 def writetags():
     tag_list = tagsover(10)
     with open('titlewordgroup.json') as file:
         filedict = ujson.load(file)
         #makes sure wordgroups are in correct order because dictionaries are unordered
         wordgroups = [v for k, v in sorted(filedict.items())]
-        bayes(tag_list, wordgroups, 0, 300000)
-        bayes(tag_list, wordgroups, 300000, 600000)
-        bayes(tag_list, wordgroups, 600000, 900000)
-        bayes(tag_list, wordgroups, 900000, 1200000)
-        bayes(tag_list, wordgroups, 1200000, 1500000)
-        bayes(tag_list, wordgroups, 1500000, 1800000)
-writetags()
-#print(chosentags()['6034218'])
+        bayes(tag_list, wordgroups, 0, 100)
+#writetags()
 '''if __name__ == "__main__":
     with open('Test.csv') as csvfile:
         csvreader = csv.reader(csvfile)
@@ -61,25 +49,10 @@ writetags()
                          'code', 'over', 'under', 'move', 'remove']
             worddict = {}
             for i, row in enumerate(csvreader):
-                words = set(str(row[1]).lower().replace('"', '')
+                words = set(str(row[2]).lower().replace('"', '')
                             .replace('.', '').replace(';', '').replace(',', '')
                             .replace('?', '').replace(';', '').replace(':', '').split())
-                worddict[row[0]] = [word for word in words if word not in stopwords and word in bodywords]
+                worddict[row[0]] = [word for word in words if word not in stopwords and '<code>' in word]
                 print(i)
-            with open('titlewordgroup.json', 'w') as write:
+            with open('codewordgroup.json', 'w') as write:
                 ujson.dump(worddict, write)'''
-
-
-def create_submission():
-    with open('submission2.csv', 'w') as submission:
-        tagdict = chosentags()
-        tagdict2 = chosentags2()
-        submission.write(','.join(['"Id"', '"Tags"']) + "\n")
-        for id in range(6034196, 7034196):
-            submission.write(','.join([str(id), "\"" + ' '.join(tagdict[str(id)]) + "\""]) + "\n")
-            print(id)
-        for id in range(7034196, 8047533):
-            submission.write(','.join([str(id), "\"" + ' '.join(tagdict2[str(id - 1000000)]) + "\""]) + "\n")
-            print(id)
-
-#create_submission()
