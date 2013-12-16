260ebe73ae2ee0221245a1c24c5035e3214cc215
Louis Impagliazzo <imp@gliazzo.com>
2013-12-16 13:00:07 -0600

    added maxscore line

--- filtercsv.py
+++ filtercsv.py
@@ -3,19 +3,20 @@ import math
 
 
 def filter_csv():
-    with open('cotagsub16.csv') as file:
-        with open('newsubmission160.csv', 'a') as writefile:
+    with open('submission07entropy.csv') as file:
+        with open('newsubmission07e.csv', 'a') as writefile:
             csvreader = csv.reader(file)
             for row in csvreader:
                 tagvaluelist = row[1].split(' ')
                 tags = tagvaluelist[:math.floor(len(tagvaluelist)/2)]
                 scores = [float(score) for score in tagvaluelist[math.floor(len(tagvaluelist)/2):]]
                 averagescore = sum(scores)/len(scores)
-                chosentags = [tags[i] for i in range(len(tags)) if scores[i] >= averagescore/2]
+                maxscore = max(scores)
+                chosentags = [tags[i] for i in range(len(tags)) if scores[i] >= 1.3*averagescore]
                 writefile.write(','.join([str(row[0]), "\""
                                           + ' '.join(chosentags) + "\""]) + "\n")
                 print(row[0])
-
+filter_csv()
 
 def tags_values():
     with open('submission07entropy.csv') as file:
