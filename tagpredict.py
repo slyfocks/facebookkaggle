eee282c87e61439929a5257f15c622337f5b6746
Louis Impagliazzo <imp@gliazzo.com>
2013-12-20 20:00:59 -0600

    cleaner final file

--- tagpredict.py
+++ tagpredict.py
@@ -34,28 +34,7 @@ def writetags():
         filedict = ujson.load(file)
         #makes sure wordgroups are in correct order because dictionaries are unordered
         wordgroups = [v for k, v in sorted(filedict.items())]
-        bayes(tag_list, wordgroups, 1995316, 2013337)
-writetags()
-'''if __name__ == "__main__":
-    with open('Test.csv') as csvfile:
-        csvreader = csv.reader(csvfile)
-        with open('titleglobalcount.json') as wordcount:
-            bodywords = set(ujson.load(wordcount).keys())
-            stopwords = ['at', 'an' 'which', 'on', 'in', 'the', 'is', 'are',
-                         'of', 'my', 'from', 'a', 'or', 'and', 'for', 'do', 'did',
-                         'I', 'to', 'too', 'out', 'why', 'what', 'by', 'if', 'this',
-                         'that', 'all', 'any', 'but', 'been', 'so', 'then', 'than',
-                         'content', 'id', 'think', 'want', 'know', 'also', 'it', 'am',
-                         'code', 'over', 'under', 'move', 'remove']
-            worddict = {}
-            for i, row in enumerate(csvreader):
-                words = set(str(row[2]).lower().replace('"', '')
-                            .replace('.', '').replace(';', '').replace(',', '')
-                            .replace('?', '').replace(';', '').replace(':', '').split())
-                worddict[row[0]] = len([word for word in words if word not in stopwords])
-                print(i)
-            with open('bodylength.json', 'w') as write:
-                ujson.dump(worddict, write)'''
+        bayes(tag_list, wordgroups, 0, 2013337)
 
 
 #updates probabilities based on code tags
